b01fd59ca33c462b2d91d3dd3b848f9a98e3b445
Miller <151492849+CilliePaint@users.noreply.github.com>
2024-08-15 20:11:55 -0400

    Mild update, simply to have something to show, even if not much.

--- init.lua
+++ init.lua
@@ -1,1 +1,17 @@
 
+
+--  FoxOS BootLoader Initialization File, do not edit.
+--  Made by Katya, violation of the above text
+--  Will result in violation of the Terms of Service, you're on your own.
+
+
+  -- Delcaring simplistic Variables, including the Gloval for Operating system version
+_G._OSVERSION = "FoxOS BootLoader 0.0.0"
+local component = component
+local computer = computer
+local unicode = unicode
+
+-- Checks for Damage to the EEPROM, and Errors, totally unnecessary but can aid in the rare case that happens.
+component = assert("[!!] [001] - Component not found! - Reinstallation of the EEPROM advised.")
+computer = assert("[!!] [002] - Computer not found! - Reinstallation of the EEPROM advised.")
+unicode = assert("[!!] [003] - Unicode not found! - Reinstallation of the EEPROM advised.")
